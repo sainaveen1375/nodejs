fbf82228111f51d6b15520c39d0546df4ab59970
naveen <sainaveen771@gmail.com>
2019-11-01 13:49:41 +0530

    changed name

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send({hi: 'Welcome'});
+    res.send({hi: 'Naveen'});
 });
 
 const PORT = process.env.PORT || 5000;
